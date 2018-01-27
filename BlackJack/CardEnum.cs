9729f66d15a58da21a3d09719ee8db5dd73d7d9f
Unknown <gabebarber94@gmail.com>
2018-01-26 19:08:24 -0700

    CardEnum
    Created the enum for the cards

---
+++ BlackJack/CardEnum.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class CardEnum
+    {
+        enum Deck{
+            AceOfHearts,
+            TwoOfHearts,
+            ThreeOfHearts,
+            FourOfHearts,
+            FiveOfHearts,
+            SixOfHearts,
+            SevenOfHearts,
+            EightOfHearts,
+            NineOfHearts,
+            TenOfHearts,
+            JackOfHearts,
+            QueenOfHearts,
+            KingOfHearts,
+            AceOfDiamonds,
+            TwoOfDiamonds,
+            ThreeOfDiamonds,
+            FourOfDiamonds,
+            FiveOfDiamonds,
+            SixOfDiamonds,
+            SevenOfDiamonds,
+            EightOfDiamonds,
+            NineOfDiamonds,
+            TenOfDiamonds,
+            JackOfDiamonds,
+            QueenOfDiamonds,
+            KingOfDiamonds,
+            AceOfSpades,
+            TwoOfSpades,
+            ThreeOfSpades,
+            FourOfSpades,
+            FiveOfSpades,
+            SixOfSpades,
+            SevenOfSpades,
+            EightOfSpades,
+            NineOfSpades,
+            TenOfSpades,
+            JackOfSpades,
+            QueenOfSpades,
+            KingOfSpades,
+            AceOfClovers,
+            TwoOfClovers,
+            ThreeOfClovers,
+            FourOfClovers,
+            FiveOfClovers,
+            SixOfClovers,
+            SevenOfClovers,
+            EightOfClovers,
+            NineOfClovers,
+            TenOfClovers,
+            JackOfClovers,
+            QueenOfClovers,
+            KingOfClovers
+
+        };
+        
+
+    }
+}
