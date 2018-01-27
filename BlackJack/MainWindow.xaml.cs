8ca953a031ec370d34736be5c3c52cf43ae2b6aa
Unknown <jordon.bowden@gmail.com>
2018-01-26 18:52:11 -0700

    Added player banks, started work on if player is too poor to play

--- BlackJack/MainWindow.xaml.cs
+++ BlackJack/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace BlackJack
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int Player1Bank = 20;
+        public int Player2Bank = 20;
+        public int Player3Bank = 20;
+        public int Player4Bank = 20;
+        public int Player5Bank = 20;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void CheckIfBankrupt()
+        {
+            if(Player1Bank < -50)
+            {
+                //in all of these, disable their option to go into the table, disable their play
+            }
+            if (Player2Bank < -50)
+            {
+
+            }
+            if (Player3Bank < -50)
+            {
+
+            }
+            if (Player4Bank < -50)
+            {
+
+            }
+            if (Player5Bank < -50)
+            {
+
+            }
         }
     }
 }
