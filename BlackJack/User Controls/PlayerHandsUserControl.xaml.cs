64d4ec161634a1d911d3ab88a818d8f66476ce90
Unknown <mbrannen747@yahoo.com>
2018-01-30 18:32:12 -0700

    Butterscotch
    (Added properties for the UserControl and added xnames to the stackpanels for the bid buttons)

--- BlackJack/User Controls/PlayerHandsUserControl.xaml.cs
+++ BlackJack/User Controls/PlayerHandsUserControl.xaml.cs
@@ -20,6 +20,31 @@ namespace BlackJack.User_Controls
     /// </summary>
     public partial class PlayerHandsUserControl : UserControl
     {
+        public ImageSource Slot1
+        {
+            get { return slot1.Source; }
+            set { slot1.Source = value; }
+        }
+        public ImageSource Slot2
+        {
+            get { return slot2.Source; }
+            set { slot2.Source = value; }
+        }
+        public ImageSource Slot3
+        {
+            get { return slot3.Source; }
+            set { slot3.Source = value; }
+        }
+        public ImageSource Slot4
+        {
+            get { return slot4.Source; }
+            set { slot4.Source = value; }
+        }
+        public ImageSource Slot5
+        {
+            get { return slot5.Source; }
+            set { slot5.Source = value; }
+        }
         public PlayerHandsUserControl()
         {
             InitializeComponent();
